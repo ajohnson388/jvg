8c3098cb25f88c62a73d9977a3c46765965135e0
Tristan Moss <tristanpmoss@gmail.com>
2021-10-04 21:21:33 -0500

    Nest Triangle class in Renderer

--- javagame/src/javagame/engine/Renderer.java
+++ javagame/src/javagame/engine/Renderer.java
@@ -7,6 +7,20 @@ import javagame.model.GameObject;
 
 public class Renderer {
 	
+	class Triangle {
+		
+		float x1, y1, x2, y2, x3, y3;
+		
+		Triangle(float x1, float y1, float x2, float y2, float x3, float y3) {
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
+			this.x3 = x3;
+			this.y3 = y3;
+		}
+	}
+	
 	public void clear() {
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 	}
